0e9cb9ba7464d3e1708ab6d9e953c0e2824a9a9c
Samvel Aivazian <92787830+samvel-aivazian@users.noreply.github.com>
2024-07-13 22:19:34 +0100

    YUR-114: [MVP 1.3] Comment Logic related to Email Confirmation

--- src/main/java/com/yurjinia/auth/controller/AuthController.java
+++ src/main/java/com/yurjinia/auth/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.yurjinia.auth.controller.request.LoginRequest;
 import com.yurjinia.auth.controller.request.RegistrationRequest;
 import com.yurjinia.auth.dto.PasswordResetDTO;
 import com.yurjinia.auth.dto.PasswordResetRequest;
-import com.yurjinia.auth.dto.ResendConfirmationRequest;
 import com.yurjinia.auth.service.AuthService;
 import com.yurjinia.common.security.jwt.dto.JwtAuthenticationResponse;
 import jakarta.validation.Valid;
@@ -31,14 +30,9 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(value = "/sign-up", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> signUp(@RequestPart("registrationRequest") RegistrationRequest registrationRequest,
-                                    @RequestPart(value = "image", required = false) MultipartFile image) {
-        try {
-            String response = authService.signUp(registrationRequest, image);
-            return ResponseEntity.ok(response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public JwtAuthenticationResponse signUp(@RequestPart("registrationRequest") RegistrationRequest registrationRequest,
+                                            @RequestPart(value = "image", required = false) MultipartFile image) {
+        return authService.signUp(registrationRequest, image);
     }
 
     @PostMapping("/login")
@@ -68,20 +62,26 @@ public class AuthController {
         authService.resetPassword(token, passwordResetDTO);
     }
 
-    @GetMapping("/sign-up/confirm")
-    public String confirmSignUp(@RequestParam("token") String token) {
-        authService.confirmSignUp(token);
-        return "Sign-up confirmed successfully!";
-    }
+    /*
+        ToDo: Refer to next JIRA with having more clarification about the reasons of
+         why the code was commented, and when it's going to be uncommented:
+         https://pashka1clash.atlassian.net/browse/YUR-114
 
-    @PostMapping("/sign-up/resend-confirmation")
-    public ResponseEntity<String> resendConfirmation(@RequestBody ResendConfirmationRequest request) {
-        try {
-            String response = authService.resendConfirmation(request.getEmail());
-            return ResponseEntity.ok(response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        @GetMapping("/sign-up/confirm")
+        public String confirmSignUp(@RequestParam("token") String token) {
+            authService.confirmSignUp(token);
+            return "Sign-up confirmed successfully!";
         }
-    }
+
+        @PostMapping("/sign-up/resend-confirmation")
+        public ResponseEntity<String> resendConfirmation(@RequestBody ResendConfirmationRequest request) {
+            try {
+                String response = authService.resendConfirmation(request.getEmail());
+                return ResponseEntity.ok(response);
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
+    */
 
 }
--- src/main/java/com/yurjinia/project_structure/project/controller/ProjectController.java
+++ src/main/java/com/yurjinia/project_structure/project/controller/ProjectController.java
@@ -1,15 +1,12 @@
 package com.yurjinia.project_structure.project.controller;
 
 import com.yurjinia.project_structure.project.dto.ProjectDTO;
-import com.yurjinia.project_structure.project.dto.ProjectInvitationDTO;
 import com.yurjinia.project_structure.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,17 +21,22 @@ public class ProjectController {
         return projectService.createProject(userEmail, projectDTO);
     }
 
-    @PostMapping("{projectName}/invite")
-    public void inviteUserToTheProject(@PathVariable String projectName,
-                                       @RequestBody ProjectInvitationDTO projectInvitationDTO) {
-        projectService.inviteUserToTheProject(projectName, projectInvitationDTO);
-    }
+    /* ToDo: Refer to next JIRA with having more clarification about the reasons of
+        why the code was commented, and when it's going to be uncommented:
+        https://pashka1clash.atlassian.net/browse/YUR-114
 
-    @GetMapping("/confirm")
-    public String confirmInvite(@RequestParam("token") String token) {
-        projectService.confirmInvite(token);
+        @PostMapping("{projectName}/invite")
+        public void inviteUserToTheProject(@PathVariable String projectName,
+                                           @RequestBody ProjectInvitationDTO projectInvitationDTO) {
+            projectService.inviteUserToTheProject(projectName, projectInvitationDTO);
+        }
 
-        return "User added to the project";
-    }
+        @GetMapping("/confirm")
+        public String confirmInvite(@RequestParam("token") String token) {
+            projectService.confirmInvite(token);
 
+            return "User added to the project";
+        }
+
+     */
 }
