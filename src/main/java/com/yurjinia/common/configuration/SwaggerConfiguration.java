cc023294d34dcd62566bcbd0fa80d509679e54a0
Dmytro Sydoruk <1303dimitri@gmail.com>
2024-08-05 10:40:10 +0300

    YUR-141: Fixed HttpMediaTypeNotSupportedException: Content-Type 'application/octet-stream' is not supported

--- src/main/java/com/yurjinia/common/configuration/SwaggerConfiguration.java
+++ src/main/java/com/yurjinia/common/configuration/SwaggerConfiguration.java
@@ -7,9 +7,19 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
 
 @Configuration
 public class SwaggerConfiguration {
+
+    public SwaggerConfiguration(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
 
     @Bean
     GroupedOpenApi publicApi() {
@@ -36,5 +46,6 @@ public class SwaggerConfiguration {
                 })
                 .build();
     }
+
 }
 
