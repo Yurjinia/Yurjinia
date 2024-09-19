fda3f37cb599da3976d0564be1d50d2e4f7eaa80
Dmytro Sydoruk <1303dimitri@gmail.com>
2024-09-19 17:32:12 +0300

    YUR-101: Fixed PR conflict

--- src/main/java/com/yurjinia/project_structure/comment/dto/CommentDTO.java
+++ src/main/java/com/yurjinia/project_structure/comment/dto/CommentDTO.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
 @Setter
 public class CommentDTO {
-    private UUID id;
+    private String id;
     private String text;
     private LocalDateTime created;
     private LocalDateTime updated;
